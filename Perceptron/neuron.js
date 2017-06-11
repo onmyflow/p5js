22b6741d4bd0428230be6e1c776a3719eb7f92e0
Daniel1998 <`b98yK57>
2017-06-11 10:39:00 +0200

    xc

--- Perceptron/neuron.js
+++ Perceptron/neuron.js
@@ -1,28 +1,32 @@
-function Perceptron(bias){
+function Perceptron(bias = 1, lr = 0.1, count = 2){
   this.errTotal = 0
-  this.w1 = random(-1,1)
-  this.w2 = random(-1,1)
+  this.weight = []
   this.biasWeight = random(-1,1)
-
-
+  this.lr = lr
   this.bias = bias
+  for(i = 0; i < count; i++){
+    this.weight[i] = random(-1,1)
+  }
+  console.log(this.weight)
 
-
-  Perceptron.prototype.guess = function(i1,i2){
-    var sum;
-    sum = i1 * this.w1 + i2 * this.w2 + this.bias * this.biasWeight
+  Perceptron.prototype.guess = function(input){
+    var sum = 0;
+    for(i=0; i< input.length; i++){
+      sum += input[i] * this.weight[i]
+    }
+    sum += this.bias * this.biasWeight
+    // console.log(sum)
     return this.activate(sum)
   }
-
-  Perceptron.prototype.train = function(i1, i2, desire){
-    var guess = this.guess(i1,i2, bias)
+  Perceptron.prototype.train = function(input, desire){
+    var guess = this.guess(input)
     var error = desire - guess
     this.errTotal += error
-    this.w1 += error * i1 * lr
-    this.w2 += error * i2 * lr
-    this.biasWeight += error * this.bias * lr
+    for(i=0;i<input.length;i++){
+      this.weight[i] += error * input[i] * this.lr
+    }
+    this.biasWeight += error * this.bias * this.lr
   }
-
   Perceptron.prototype.activate = function(input){
     if(input >= 0){
       return 1
@@ -30,5 +34,4 @@ function Perceptron(bias){
       return -1
     }
   }
-
 }
--- Perceptron/sketch.js
+++ Perceptron/sketch.js
@@ -5,47 +5,59 @@ var lr = 0.01
 
 
 function setup(){
-  translate(200,200)
+
   createCanvas(500,500)
   background(51)
   stroke(255)
   strokeWeight(1)
-for(i=0;i < 100000; i++){
-  var xsol = random(width)
-  var ysol = random(height)
-  var out = -1
-  if(ysol > f(xsol)){
-    out = 1
-  }
-  solution[i] = {
-    x:xsol,
-    y:ysol,
-    output: out
-  }
-}
-  p1 = new Perceptron(200)
-  for(i = 0; i < solution.length; i++){
-    p1.train(solution[i].x, solution[i].y,  solution[i].output)
-  }
-  console.log(p1.errTotal)
-  for(i = 0; i < solution.length; i++){
-    if(p1.guess(solution[i].x,solution[i].y) == 1){
-        fill(0,255,0)
-      } else if(p1.guess(solution[i].x,solution[i].y,) == -1){
-        fill(255,0,0)
-      }
-    ellipse(solution[i].x,solution[i].y,10,10)
-  }
-  console.log(p1.w1, p1.w2, p1.biasWeight)
-  line(0,f(0),width,f(width))
+// for(i=0;i < 100000; i++){
+//   var xsol = random(width)
+//   var ysol = random(height)
+//   var out = -1
+//   if(ysol > f(xsol)){
+//     out = 1
+//   }
+//   solution[i] = {
+//     x:xsol,
+//     y:ysol,
+//     output: out
+//   }
+// }
+  p1 = new Perceptron(width, lr, 2)
+  // for(i = 0; i < solution.length; i++){
+  //
+  // }
+  // console.log(p1.errTotal)
+  // for(i = 0; i < solution.length; i++){
+  //
+  // }
+  // console.log(p1.w1, p1.w2, p1.biasWeight)
 }
 function draw(){
-  
+// translate(width/2, height/2)
+var randomX = random(width)
+var randomY = random(height)
+var out
+if(randomY >= f(randomX)){
+  out = 1
+} else {
+  out = -1
+}
+
+  p1.train([randomX, randomY],  out)
+  if(p1.guess([randomX, randomY]) == 1){
+      fill(0,255,0)
+    } else {
+      fill(255,0,0)
+    }
+  var xMap = randomX
+  var yMap = map(randomY,0,height,height,0)
+  ellipse(xMap, yMap,10,10)
 }
 function mousePressed(){
 
 }
 
 function f(x){
-  return 0.2*x+200
+  return 0.3*x+300
 }
